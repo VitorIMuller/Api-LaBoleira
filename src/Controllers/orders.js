f60488d2562916baf1f2e566f1d7343eaa0b3630
Vitor <vitormuller66@gmail.com>
2022-03-18 15:50:54 -0300

    feat: get orders ok

--- src/Controllers/orders.js
+++ src/Controllers/orders.js
@@ -21,3 +21,64 @@ export async function postOrders(req, res) {
         res.status(500).send(error.message)
     }
 }
+
+export async function getOrders(req, res) {
+
+    try {
+        const { rows: orders } = await connection.query({
+            text: `
+            SELECT 
+                c.id AS "clientId", c.name AS "clientName", c.address AS "clientAddress", c.phone AS "clientPhone",
+                ca.id AS "cakeId", ca.name AS "cakeName", ca.price AS "cakePrice", ca.description AS "cakeDescription", ca.image AS "cakeImage",
+                o."createAt" AS createdAt, o.quantity AS "orderQuantity", o."totalPrice" AS "orderTotalPrice"
+            FROM
+                orders o
+            JOIN clients c ON c.id = o."clientId"
+            JOIN cakes ca ON ca.id = o."cakeId"`
+            , rowMode: "array"
+        });
+        if (orders.rowCount === 0) return res.status(404).send([])
+        res.send(
+            orders.map((row) => {
+                const [
+                    clientId,
+                    clientName,
+                    clientAddress,
+                    clientPhone,
+                    cakeId,
+                    cakeName,
+                    cakePrice,
+                    cakeDescription,
+                    cakeImage,
+                    createdAt,
+                    orderQuantity,
+                    orderTotalPrice
+                ] = row
+
+                return {
+                    client: {
+                        id: clientId,
+                        name: clientName,
+                        address: clientAddress,
+                        phone: clientPhone
+                    },
+                    cake: {
+                        id: cakeId,
+                        name: cakeName,
+                        price: cakePrice,
+                        description: cakeDescription,
+                        image: cakeImage
+                    },
+                    createdAt: createdAt,
+                    quantity: orderQuantity,
+                    totalPrice: orderTotalPrice
+                }
+            })
+
+
+        )
+    } catch (error) {
+        console.log(error.message)
+        res.sendStatus(500)
+    }
+}
--- src/Router/ordersRouter.js
+++ src/Router/ordersRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { postOrders } from "../Controllers/orders.js";
+import { getOrders, postOrders } from "../Controllers/orders.js";
 import validateOrderMiddleware from "../Middlewares/validateOrderMIddleware.js";
 
 
 const ordersRouter = Router();
 
 ordersRouter.post("/order", validateOrderMiddleware, postOrders);
-
+ordersRouter.get("/orders", getOrders)
 export default ordersRouter
