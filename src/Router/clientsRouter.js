b2cf431fb3a6ebd05adea1f4aaaec2c95bbc233b
Vitor <vitormuller66@gmail.com>
2022-03-18 16:40:44 -0300

    feat: get order by client id

--- src/Router/clientsRouter.js
+++ src/Router/clientsRouter.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { postClients } from "../Controllers/clients.js";
+import { getOrdersByClientId, postClients } from "../Controllers/clients.js";
 import validateClientMiddleware from "../Middlewares/validateClientMIddleware.js";
 
 
 const clientsRouter = Router();
 
 clientsRouter.post("/clients", validateClientMiddleware, postClients)
-
+clientsRouter.get("/clients/:id/orders", getOrdersByClientId)
 
 
 export default clientsRouter;
